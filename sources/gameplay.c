88d13ff692702196c8a4936d37d5f99ce5a7a775
Samael <jeremie.joannard@epitech.eu>
2024-09-18 05:49:38 +0200

    Adding player movement
    [MOD] sources/gameplay.c,

--- sources/gameplay.c
+++ sources/gameplay.c
@@ -26,14 +26,64 @@ int print_map_ncurses(map_t *map, Point start, Point end, Point player)
     return 0;
 }
 
+int move_player(map_t *map, Point *player, Point end)
+{
+    int ch = getch();
+    switch (ch) {
+        case 'z':
+        case 'Z':
+            if (player->y - 1 >= 0 && map->map[player->y - 1][player->x] == 1) {
+                player->y -= 1;
+            }
+            break;
+        case 's':
+        case 'S':
+            if (player->y + 1 < map->size_y && map->map[player->y + 1][player->x] == 1) {
+                player->y += 1;
+            }
+            break;
+        case 'q':
+        case 'Q':
+            if (player->x - 1 >= 0 && map->map[player->y][player->x - 1] == 1) {
+                player->x -= 1;
+            }
+            break;
+        case 'd':
+        case 'D':
+            if (player->x + 1 < map->size_x && map->map[player->y][player->x + 1] == 1) {
+                player->x += 1;
+            }
+            break;
+        case 27: // ESC key
+            return 2;
+            break;
+        default:
+            break;
+    }
+    return 0;
+}
+
+int is_same_point(Point a, Point b)
+{
+    if (a.x == b.x && a.y == b.y) {
+        return 1;
+    }
+    return 0;
+}
+
 
 int gameplay(map_t *map, Point start, Point end)
 {
     initscr();
     noecho();
     bool is_end = false;
+    Point player = start;
     while(is_end != true) {
-        print_map_ncurses(map, start, end, start);
+        print_map_ncurses(map, start, end, player);
+        move_player(map, &player, end);
+        if (is_same_point(player, end)) {
+            is_end = true;
+        }
         refresh();
     }
     endwin();
